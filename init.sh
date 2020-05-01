c596029efbe6819b2a5bfe0bd4463756f6911b92
Tim Veil <3260845+timveil@users.noreply.github.com>
2020-05-01 17:53:30 -0400

    fixing grant

--- init.sh
+++ init.sh
@@ -42,5 +42,5 @@ fi
 if [ -n "$DATABASE_USER" ] && [ -n "$DATABASE_PASSWORD" ] && [ -n "$DATABASE_NAME" ]; then
   echo "found DATABASE_USER [${DATABASE_USER}] and DATABASE_PASSWORD [${DATABASE_PASSWORD}], creating..."
   ./cockroach sql --execute="CREATE USER ${DATABASE_USER} WITH PASSWORD '${DATABASE_PASSWORD}';"
-  ./cockroach sql --execute="GRANT CREATE, INSERT, DELETE, UPDATE, SELECT ON ${DATABASE_NAME} TO ${DATABASE_USER};"
+  ./cockroach sql --execute="GRANT ALL ON DATABASE ${DATABASE_NAME} TO ${DATABASE_USER};"
 fi
